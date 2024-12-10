20a9e30c5659f7f86329f4f88f0ebb5372c57343
SajjadBayat <sajjad.bayat32@gmail.com>
2024-12-11 00:04:06 +0330

    feat: tow routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'search',
+    component: SearchPageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full',
+  },
+];
